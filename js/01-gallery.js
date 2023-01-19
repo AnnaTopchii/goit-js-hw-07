f497bfd0bc5bf1ecfca229f75b64ee284e471b2c
AnnaTopchii <113939011+AnnaTopchii@users.noreply.github.com>
2023-01-19 18:02:39 +0200

    1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,61 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryCards = createGallery(galleryItems);
+
+
+
+galleryContainer.innerHTML = galleryCards;
+galleryContainer.addEventListener('click', onSmallImageClick) ;
+
+//console.log(galleryCards);
+
+
+
+function createGallery(items) {
+    return items.map(({ preview, original, description }) => 
+    
+        `<div class="gallery__item">
+         <a class="gallery__link" href="${original}">
+         <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+         />
+        </a>
+        </div>`
+    )
+        .join('');
+};
+
+function onSmallImageClick(evt) {
+    evt.preventDefault();
+    console.log("Some img clicked");
+    const isImage = evt.target.classList.contains('gallery__image');
+    if (!isImage) {
+        return;
+    }
+
+    const urlOfBigImage = evt.target.dataset.source;
+    
+    openModalBigImage(urlOfBigImage);
+}
+
+function openModalBigImage(url) {
+    
+    const bigImage = basicLightbox.create(`
+		<img src="${url}">`,
+        { onShow: (bigImage) =>
+                document.addEventListener('keydown', function close(evt) {
+                    console.log("Some key was pressed");
+            if (evt.code === 'Escape') { bigImage.close(); }
+            }
+          ),
+          onClose: (bigImage) => document.removeEventListener('keydown', close)
+        })
+        .show();
+}  
+	
